c31ee2a06a39fd5e9e96fc894ece7e1ef2937a83
Aashay Tiwari <tiwari.aa@husky.neu.edu>
2020-06-14 04:22:59 -0400

    image name changed

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/About.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Aashay Tiwari</a></h1>
               <span className="email"><i className="icon-mail"></i> tiwari.aa@husky.neu.edu</span>
               <span className="email"><i className="icon-phone"></i> +1-8578698680</span>
