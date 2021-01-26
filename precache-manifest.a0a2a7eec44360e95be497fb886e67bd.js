e26ec099c29247560f95bb872c9fd8c19ec4c9bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-26 04:25:28 +0000

    Deploy aashay94/portfolio to github.com/aashay94/portfolio.git:gh-pages

---
+++ precache-manifest.a0a2a7eec44360e95be497fb886e67bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5c34d9c06900e3a9c5d",
+    "url": "/portfolio/static/css/main.0b1ec4eb.chunk.css"
+  },
+  {
+    "revision": "a5c34d9c06900e3a9c5d",
+    "url": "/portfolio/static/js/main.a5c34d9c.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2287bc54aac8d1d6307eece32fe41eca",
+    "url": "/portfolio/index.html"
+  }
+];
